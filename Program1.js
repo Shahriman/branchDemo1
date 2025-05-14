e883c058956e280a1eb7410e461c94860fddbf9f
Shahriman Mohd Said <shahriman.said@newinti.edu.my>
2025-05-14 13:39:51 +0800

    Updating Again

--- Program1.js
+++ Program1.js
@@ -1,8 +1,8 @@
-//Update by Shahriman on 14 May 2025, at 9.30am.
+//Update by Shahriman on 14 May 2025, at 1.40pm.
 
 const axios = require('axios');
 const apikey = 'AIzaSyAn7h3tsW_p0md5iISNFzLcJDoRGRgjWPg';
-const address = 'Queensbay Mall';
+const address = 'USM Penang';
 
 const querystr = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${apikey}`;
 
